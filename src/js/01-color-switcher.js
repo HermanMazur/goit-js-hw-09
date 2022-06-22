9af4b005063e6a5e3ad19d0484d15c237dba9060
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-22 15:33:29 +0300

    1111

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,9 +5,24 @@ const bodyElement = document.querySelector('body');
 startButton.addEventListener('clck', onClickStartButton);
 stopButton.addEventListener('clck', onClickStopButton);
 
+let timerId = null;
+stopButton.setAttribute('disabled', true);
+
+
 function onClickStartButton() {
+    timerId = setInterval(() => {
+        bodyElement.style.background = getRandomHexColor();
+    }, 1000);
+    startButton.setAttribute('disabled', true);
+    stopButton.removeAttribute('disabled');
 }
 
+
+function onClickStopButton() {
+    clearInterval(timerId);
+    startButton.removeAttribute('disabled');
+    startButton.setAttribute('disabled', true)
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
