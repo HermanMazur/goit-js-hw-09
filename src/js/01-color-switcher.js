61b6e5885b5040c179ea2df90c3bb42d40dcb8a2
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-22 15:19:43 +0300

    1 t

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const bodyElement = document.querySelector('body');
 
+startButton.addEventListener('clck', onClickStartButton);
+stopButton.addEventListener('clck', onClickStopButton);
+
+function onClickStartButton() {
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
