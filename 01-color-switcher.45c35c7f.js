017ec38b2ee9d551b1e004ae48144ab5b1c12f78
HermanMazur <95304946+HermanMazur@users.noreply.github.com>
2022-06-22 12:34:14 +0000

    Deploying to gh-pages from @ HermanMazur/goit-js-hw-09@9af4b005063e6a5e3ad19d0484d15c237dba9060 🚀

--- 01-color-switcher.45c35c7f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("clck",(function(){})),e.addEventListener("clck",onClickStopButton)}();
-//# sourceMappingURL=01-color-switcher.45c35c7f.js.map
