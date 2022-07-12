dc4512b4eccffa7209030a99d5f363384d7aac06
color <95304946+HermanMazur@users.noreply.github.com>
2022-07-12 14:11:29 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('click', onFormSubmit)
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +11,16 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+}
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
